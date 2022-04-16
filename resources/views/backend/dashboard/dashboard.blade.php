641b3ad5abc4c9f09fa2cedb816375ce4b18c1bb
inheritance10 <ali552001a@gmail.com>
2022-04-16 20:52:47 +0300

    Initial commit

---
+++ resources/views/backend/dashboard/dashboard.blade.php
@@ -0,0 +1,15 @@
+@extends('backend.layout')
+@section('content')
+    <div class="page-breadcrumb bg-white">
+        <div class="row align-items-center">
+            <div class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
+                <h4 class="page-title">Anasayfa</h4>
+            </div>
+        </div>
+    </div>
+@endsection
+@section('css')
+@endsection
+
+@section('js')
+@endsection
